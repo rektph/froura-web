7887fb2c1c3c3a773ed7811a7d3b98545c0b0911
rektph <kendrickjaviercosca@gmail.com>
2018-10-21 01:13:28 +0800

    Saved some changes

--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -6,10 +6,21 @@ import router from './router'
 import Vuetify from 'vuetify';
 import store from './store'
 
+<<<<<<< HEAD
 const baseUrl = 'http://pos.test'
 
 Vue.use(Vuetify)
 
+=======
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#F9A825',
+    secondary: '#FFB300',
+    accent: '#8c9eff',
+    error: '#b71c1c'
+  }
+})
+>>>>>>> Saved some changes
 Vue.config.productionTip = false
 
 
